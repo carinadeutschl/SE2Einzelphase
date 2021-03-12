3565c6853a36b3350ddf990bdda1b4cbd10f6d56
carinadeutschl <carinadeu@edu.aau.at>
2021-03-12 11:04:47 +0100

    first implementations of main activity

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.se2einzelphase;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtStudentid;
+    TextView txtAnswer;
+    Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtStudentid = findViewById(R.id.txtStudentid);
+        txtAnswer = findViewById(R.id.txtAnswer);
+        btnSend = findViewById(R.id.btnSend);
     }
+
 }
